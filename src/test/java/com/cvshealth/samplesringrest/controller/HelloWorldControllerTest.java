8401554090e2f6ebaa0f1a70aeaec9fdb94caa69
Sadriddin Norkobilov <sadir_dip@yahoo.com>
2022-12-13 14:58:43 -0600

    added test

---
+++ src/test/java/com/cvshealth/samplesringrest/controller/HelloWorldControllerTest.java
@@ -0,0 +1,15 @@
+package com.cvshealth.samplesringrest.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldControllerTest {
+    HelloWorldController helloWorldController = new HelloWorldController();
+
+    @Test
+    void helloWorld() {
+        String expectedWord = helloWorldController.helloWorld();
+        assertEquals("HELLO WORLD", expectedWord);
+    }
+}
