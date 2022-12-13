9a6ed1237a09ce6dff08a386a2ff8d272e3d6532
Sadriddin Norkobilov <sadir_dip@yahoo.com>
2022-12-13 13:44:47 -0600

    Initial commit

---
+++ src/main/java/com/cvshealth/samplesringrest/SampleSringRestApplication.java
@@ -0,0 +1,13 @@
+package com.cvshealth.samplesringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSringRestApplication.class, args);
+    }
+
+}
