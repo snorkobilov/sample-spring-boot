b48dd12ebb9f5a01695bf9d7f74df3c0dcd4d10e
Sadriddin Norkobilov <sadriddin.norkobilov@cvshealth.com>
2023-07-21 15:43:44 -0500

    wip

--- src/test/java/com/cvshealth/samplespringrest/integration/HelloWorldIntegrationTest.java
+++ src/test/java/com/cvshealth/samplespringrest/integration/HelloWorldIntegrationTest.java
@@ -1,50 +1,50 @@
-package com.cvshealth.samplespringrest.integration;
-
-import com.cvshealth.samplespringrest.repository.HelloWorldEntity;
-import com.cvshealth.samplespringrest.repository.HelloWorldRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testcontainers.containers.OracleContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@Testcontainers
-public class HelloWorldIntegrationTest {
-
-
-    @Container
-    public static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:slim");
-
-    @DynamicPropertySource
-    public static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", oracleContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", oracleContainer::getUsername);
-        registry.add("spring.datasource.password", oracleContainer::getPassword);
-        registry.add("spring.datasource.driver-class-name", oracleContainer::getDriverClassName);
-    }
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    HelloWorldRepository helloWorldRepository;
-
-    @Test
-    void returnsHelloWorld() throws Exception {
-        helloWorldRepository.save(new HelloWorldEntity(1L, "HELLO WORLD"));
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello-world"))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("HELLO WORLD"));
-    }
-}
+//package com.cvshealth.samplespringrest.integration;
+//
+//import com.cvshealth.samplespringrest.repository.HelloWorldEntity;
+//import com.cvshealth.samplespringrest.repository.HelloWorldRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//import org.testcontainers.containers.OracleContainer;
+//import org.testcontainers.junit.jupiter.Container;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@Testcontainers
+//public class HelloWorldIntegrationTest {
+//
+//
+//    @Container
+//    public static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:slim");
+//
+//    @DynamicPropertySource
+//    public static void overrideProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", oracleContainer::getJdbcUrl);
+//        registry.add("spring.datasource.username", oracleContainer::getUsername);
+//        registry.add("spring.datasource.password", oracleContainer::getPassword);
+//        registry.add("spring.datasource.driver-class-name", oracleContainer::getDriverClassName);
+//    }
+//
+//    @Autowired
+//    MockMvc mockMvc;
+//
+//    @Autowired
+//    HelloWorldRepository helloWorldRepository;
+//
+//    @Test
+//    void returnsHelloWorld() throws Exception {
+//        helloWorldRepository.save(new HelloWorldEntity(1L, "HELLO WORLD"));
+//        mockMvc.perform(MockMvcRequestBuilders.get("/hello-world"))
+//                .andExpect(status().isOk())
+//                .andExpect(MockMvcResultMatchers.content().string("HELLO WORLD"));
+//    }
+//}
